7fde62b10e6d72aa50d987a0174476cdb3e3ef4d
Guakocius <engelhardt.alexander@mein.gmx>
2026-07-27 19:02:45 +0200

    Added dependency fix for rustdoc and the first documentation for player.rs and lib.rs

--- src/components/player.rs
+++ src/components/player.rs
@@ -1,3 +1,21 @@
+//! A module for specifying the player's core behavior.
+
+/// The speed of the player defined as a resource for re-using.
+///
+/// # Examples
+///
+/// ```
+/// use bevy::prelude::*;
+/// use home_invasion::components::player::PlayerSpeed;
+///
+/// App::new().insert_resource(PlayerSpeed(100.0));
+/// ```
+#[derive(Resource, Debug, Clone)]
+pub struct PlayerSpeed(
+    /// Player speed as a 32 bit floating point number.
+    pub f32,
+);
+
 use bevy::prelude::*;
 use bevy_camera_controller::free_camera::FreeCamera;
 
@@ -7,6 +25,7 @@ pub struct Player {
     pub radius: f32,
     pub length: f32,
     pub pos: Vec3,
+    pub speed: PlayerSpeed,
     pub free_camera: Option<FreeCamera>,
     pub camera: Option<Camera>,
 }
@@ -15,6 +34,7 @@ impl Player {
     fn new(
         radius: f32,
         length: f32,
+        speed: PlayerSpeed,
         free_camera: Option<FreeCamera>,
         camera: Option<Camera>,
     ) -> Self {
@@ -23,6 +43,7 @@ impl Player {
             radius,
             length,
             pos: Vec3::ZERO,
+            speed,
             free_camera,
             camera,
         }
@@ -35,7 +56,8 @@ fn setup(
     mut standard_materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let camera = Some(FreeCamera { ..default() });
-    let player = Player::new(0.5, 1.0, camera, None);
+    let player_speed = PlayerSpeed(100.0);
+    let player = Player::new(0.5, 1.0, player_speed.clone(), camera, None);
     let mesh = meshes.add(Capsule3d::new(player.radius, player.length / 2.0));
 
     commands.spawn((
@@ -47,8 +69,24 @@ fn setup(
         Transform::from_translation(player.pos),
         player,
     ));
+    commands.insert_resource(player_speed);
 }
 
+/// The Plugin for the [Players](home_invasion::components::player::Player) core functionalities.
+///
+/// # Examples
+///
+/// ```
+/// use bevy::prelude::*;
+/// use home_invasion::components::player::PlayerPlugin;
+///
+/// App::new()
+///     .add_plugins((MinimalPlugins, AssetPlugin::default(),
+/// PlayerPlugin))
+///     .init_asset::<Mesh>()
+///     .init_asset::<StandardMaterial>()
+///     .update();
+/// ```
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! # Home Invasion
+//!
+//! A 3D horror game using the Bevy engine in collaboration with
+//! [SQUA7426](https://github.com/SQUA7426)
+//!
+//! ## Examples
+//!
+//! ```
+//! use bevy::prelude::*;
+//! use bevy_camera_controller::free_camera::FreeCameraPlugin;
+//! use home_invasion::components::{cam::CamPlugin, house::HousePlugin};
+//!
+//! fn main() {
+//!     App::new()
+//!         .add_plugins((
+//!             (
+//!                 (DefaultPlugins, FreeCameraPlugin),
+//!                 (CamPlugin, HousePlugin)
+//!             )
+//!         ));
+//! }
+//! ```
+//!
 #![warn(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 pub mod components {
