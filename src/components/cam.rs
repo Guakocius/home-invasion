5cf789ce742e2aaf2487067259ac9c5b60e42af1
Guakocius <engelhardt.alexander@mein.gmx>
2026-08-12 22:33:56 +0200

    Replaced FreeCamera with Camera3d and attached it to the player

--- src/components/cam.rs
+++ src/components/cam.rs
@@ -1,7 +1,15 @@
 //! This module defines the core functionality of the Camera used by the Player.
 
-use bevy::prelude::*;
-use bevy_camera_controller::free_camera::FreeCamera;
+use std::f32::consts::FRAC_PI_2;
+
+use super::player::Player;
+use bevy::{
+    camera::visibility::RenderLayers, color::palettes::tailwind,
+    input::mouse::AccumulatedMouseMotion, prelude::*,
+};
+
+const DEFAULT_RENDER_LAYER: usize = 0;
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
 
 /// Plugin for adding the camera's related systems.
 ///
@@ -17,20 +25,63 @@ pub struct CamPlugin;
 
 impl Plugin for CamPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_cam_light);
+        app.add_systems(Startup, (spawn_view_model, setup_cam_light));
     }
+}
+
+#[derive(Debug, Component, Deref, DerefMut)]
+pub struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(Vec2::new(0.003, 0.002))
+    }
+}
+
+#[derive(Debug, Component)]
+struct WorldModelCamera;
+
+fn spawn_view_model(
+    mut cmds: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    cmds.spawn((
+        Player,
+        CameraSensitivity::default(),
+        Transform::from_xyz(0.0, 10.0, 0.0).looking_at(vec3(1.0, 0.0, 0.0), Vec3::Y),
+        Visibility::default(),
+        children![
+            (
+                WorldModelCamera,
+                Camera3d::default(),
+                Projection::from(PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }),
+            ),
+            (
+                Camera3d::default(),
+                Camera {
+                    order: 1,
+                    clear_color: ClearColorConfig::None,
+                    ..default()
+                },
+                Projection::from(PerspectiveProjection {
+                    fov: 70.0_f32.to_radians(),
+                    ..default()
+                }),
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            ),
+        ],
+    ));
 }
 
 fn setup_cam_light(mut cmds: Commands) {
     cmds.spawn((
         DirectionalLight::default(),
-        Transform::from_xyz(10., 10., 10.).looking_at(Vec3::ONE, Vec3::Y),
-    ));
-
-    cmds.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0., 10., 0.).looking_at(vec3(1., 0., 0.), Vec3::Y),
-        FreeCamera { ..default() },
+        Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ONE, Vec3::Y),
+        RenderLayers::from_layers(&[DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER]),
     ));
 }
 
